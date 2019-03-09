07a3439cd1def4394d9686c1b2033fb72d549b86
mrblrzl <m.s.arzola@gmail.com>
2019-03-09 15:52:16 -0800

    Updates

---
+++ precache-manifest.7d6562142eb2d61b81a4a9ed48629d38.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "59d5b3cb66ea6469409631957231da0c",
+    "url": "/baseball-hack/static/media/Pitcher_Ball.59d5b3cb.jpg"
+  },
+  {
+    "revision": "6e387845b01d77ed5b1a7919195a0aa4",
+    "url": "/baseball-hack/static/media/Batters_View.6e387845.JPG"
+  },
+  {
+    "revision": "f4393b7ae2fb22fa812f",
+    "url": "/baseball-hack/static/js/runtime~main.8f486277.js"
+  },
+  {
+    "revision": "b9272a28e2e499594fcf",
+    "url": "/baseball-hack/static/js/main.d60aaeb2.chunk.js"
+  },
+  {
+    "revision": "3791395ae6897930f09c",
+    "url": "/baseball-hack/static/js/2.b19c2b74.chunk.js"
+  },
+  {
+    "revision": "b9272a28e2e499594fcf",
+    "url": "/baseball-hack/static/css/main.38aab725.chunk.css"
+  },
+  {
+    "revision": "f898b896e7508c84a46654cf14854419",
+    "url": "/baseball-hack/index.html"
+  }
+];
